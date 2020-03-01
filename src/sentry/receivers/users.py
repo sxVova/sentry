095c9355bcbc7050b2e64c613dcc266e47dbff7f
Volodymyr Skliar <rasavo99@gmail.com>
2020-03-01 16:22:27 +0000

    Update users.py

--- src/sentry/receivers/users.py
+++ src/sentry/receivers/users.py
@@ -25,16 +25,4 @@ def create_first_user(app_config, using, interactive, **kwargs):
     if not interactive:
         return
 
-    import click
-
-    if not click.confirm("\nWould you like to create a user account now?", default=True):
-        # Not using `abort=1` because we don't want to exit out from further execution
-        click.echo("\nRun `sentry createuser` to do this later.\n")
-        return
-
-    from sentry.runner import call_command
-
-    call_command("sentry.runner.commands.createuser.createuser", superuser=True)
-
-
 post_migrate.connect(create_first_user, dispatch_uid="create_first_user", weak=False)
